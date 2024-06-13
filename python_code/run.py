1f02fbef2876e526882adc0f30d5e1252348a048
Erik Lee <leex6144@umn.edu>
2024-06-13 17:39:43 -0400

    [dev]

--- python_code/run.py
+++ python_code/run.py
@@ -283,13 +283,13 @@ def build_parser():
     parser.add_argument("json_settings", help="The path to the subject-agnostic JSON file that will be used to configure processing settings", type=str)
 
     parser.add_argument('--participant_label', '--participant-label', help="The name/label of the subject to be processed (i.e. sub-01 or 01)", type=str)
-    parser.add_argument('--segmentation_dir', '--segmentation-dir', help="The path to the folder where segmentations are stored (this is the same for all subjects)", type=str)
     parser.add_argument('--session_id', '--session-id', help="OPTIONAL: the name of a specific session to be processed (i.e. ses-01)", type=str)
+    parser.add_argument('--segmentation_dir', '--segmentation-dir', help="The path to the folder where segmentations are stored (this is the same for all subjects)", type=str)
     parser.add_argument('--localizer_registration', '--localizer-registration', help="OPTIONAL: Use localizer to register anatomical images to MRS scan. Also requires the use of --segmentation_dir argument", action='store_true')
     parser.add_argument('--localizer_search_term', '--localizer-search-term', help="OPTIONAL: The search term to use to find localizer images (i.e. *localizer*)", type=str, default='*localizer*.nii*')
     parser.add_argument('--preferred_anat_modality', '--preferred-anat-modality', help="OPTIONAL: The preferred modality to use for anatomical images (i.e. T1w or T2w)", type=str, default='T1w')
     parser.add_argument('--terms_not_allowed_in_anat', nargs='+', help='One or more terms (seperated by spaces) that are not allowed in the file name of high-res anatomical reference images. Useful for getting rid of localizer images that can be confused for high-res anatomical scans. example - "--terms_not_allowed_in_anat mrs ax coronal"', required=False)
-    parser.add_argument('--require_same_mrs_localizer_suid', help='If activated, OSPREY will only try to match MRS scans with localizers if the StudyInstanceUID field in the BIDS JSON matches across the localizer/mrs images.', action='store_true')
+    parser.add_argument('--require_same_mrs_localizer_suid', help='If activated, OSPREY will only try to match MRS scans with localizers if the StudyInstanceUID field in the BIDS JSON matches across the localizer/mrs images. Note: if the StudyInstanceUID is undefined for the MRS acquisition, denoted by a value of None, then a localizer will be chosen without taking the StudyInstanceUID into account.', action='store_true')
     
     return parser
 
